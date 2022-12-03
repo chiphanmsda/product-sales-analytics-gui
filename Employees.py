cc3188abe3f7ba2259349c38a2811e616eee3e20
chiphanmsda <phan.chi185@gmail.com>
2022-12-03 12:08:24 -0800

    Add sales of employees per location
    Add sales of employees per location

--- Employees.py
+++ Employees.py
@@ -17,15 +17,32 @@ class EmployeesDialog(QDialog):
         # Load the dialog components.
         self.ui = uic.loadUi('employees_dialog.ui')
 
+        
+        ### PER EMPLOYEE
         # Employees menu and query button event handlers.
         self.ui.employees_cb.currentIndexChanged.connect(self._initialize_table)
-        self.ui.query_button.clicked.connect(self._enter_quaterly_sales_data)
-        
-        # Initialize the teacher menu and the salesrepemployee table.
+        self.ui.query_button.clicked.connect(self._enter_sales_data)
+
+        # Initialize the employees menu and sales table.
         self._initialize_employees_menu()
         self._initialize_table()
         self.ui.monthly_radio.toggled.connect(self._initialize_table)
         self.ui.quarterly_radio.toggled.connect(self._initialize_table)
+
+
+        ### PER LOCATION
+        # Location menu and query button event handlers.
+        # When the country is changed, initialize the city menu again
+        self.ui.country_cb.currentIndexChanged.connect(self._initialize_city_menu)
+        self.ui.query_button_location.clicked.connect(self._enter_sales_data_location)
+
+        # Initialize the location menu and sales table.
+        self._initialize_country_menu()
+        self._initialize_table_location()
+        self.ui.monthly_radio_location.toggled.connect(self._initialize_table_location)
+        self.ui.quarterly_radio_location.toggled.connect(self._initialize_table_location)
+        self.ui.country_cb.currentIndexChanged.connect(self._initialize_table_location)
+        self.ui.city_cb.currentIndexChanged.connect(self._initialize_table_location)
         
     def show_dialog(self):
         """
@@ -43,10 +60,44 @@ class EmployeesDialog(QDialog):
             """
         rows, _ = do_query(sql)
 
-        # Set the menu items to the teacher names.
+        # Set the menu items to the employees' names.
         for row in rows:
             name = row[0] + ' ' + row[1]
-            self.ui.employees_cb.addItem(name, row)     
+            self.ui.employees_cb.addItem(name, row)
+    
+    def _initialize_country_menu(self):
+        """
+        Initialize the country menu of clients' location from the database.
+        """
+        sql = """
+            SELECT distinct country FROM customers
+            ORDER BY country
+            """
+        rows_country, _ = do_query(sql)
+
+        # Set the menu items to the country
+        for row in rows_country:
+            c = row[0]
+            self.ui.country_cb.addItem(c, row)
+
+    def _initialize_city_menu(self):
+        """
+        Initialize the city menu of clients' location from the database.
+        """
+        self.ui.city_cb.clear()
+        country = self.ui.country_cb.currentData()
+        _country = country[0]
+        sql = """
+            SELECT distinct city FROM customers
+            WHERE country = '""" + _country + """' ORDER BY city
+            """ 
+        
+        rows_city, _ = do_query(sql)
+
+        # Set the menu items to the city by selected country
+        for row in rows_city:
+            c = row[0]
+            self.ui.city_cb.addItem(c, row)
             
     def _adjust_column_widths(self):
         """
@@ -59,6 +110,21 @@ class EmployeesDialog(QDialog):
         header.setSectionResizeMode(3, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(4, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(5, QHeaderView.Stretch)
+
+    def _adjust_column_widths_location(self):
+        """
+        Adjust the column widths of the sales table per location
+        to fit the contents.
+        """
+        header = self.ui.sales_table_location.horizontalHeader();
+        header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(3, QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(4, QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(5, QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(6, QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(7, QHeaderView.Stretch)
         
     def _initialize_table(self):
         """
@@ -78,13 +144,32 @@ class EmployeesDialog(QDialog):
             col = col_2
         self.ui.sales_table.setHorizontalHeaderLabels(col)        
         self._adjust_column_widths()
+
+    def _initialize_table_location(self):
+        """
+        Clear the table and set the column headers.
+        """
+        self.ui.sales_table_location.clear()
+
+        col_1 = [' Country ', ' City ', ' First Name ', ' Last Name ', ' Manager Name ', ' Month ',
+        ' Year End ', ' Revenue ($000) ']
+
+        col_2 = [' Country ', ' City ', ' First Name ', ' Last Name ', ' Manager Name ', ' Quater ',
+        ' Year End ', ' Revenue ($000) ']
+
+        if self.ui.monthly_radio_location.isChecked():
+            col = col_1
+        elif self.ui.quarterly_radio_location.isChecked():
+            col = col_2
+        self.ui.sales_table_location.setHorizontalHeaderLabels(col)        
+        self._adjust_column_widths_location()
         
-    def _enter_quaterly_sales_data(self):    
+    def _enter_sales_data(self):    
         """
         Enter monthly/quaterly sales data from the query into 
         the star schema and return quaterly sales per
         each employee.
-        """    
+        """
         name = self.ui.employees_cb.currentData()
         first_name = name[0]
         last_name = name[1]
@@ -123,7 +208,7 @@ class EmployeesDialog(QDialog):
         # Set the sales data into the table cells.
         row_index = 0
         for row in rows:
-            print(row)
+            # print(row)
             column_index = 0
             i = 0
             for data in row:
@@ -138,6 +223,70 @@ class EmployeesDialog(QDialog):
             row_index += 1
                 
         self._adjust_column_widths()
+
+    def _enter_sales_data_location(self):    
+        """
+        Enter monthly/quaterly sales data from the query into 
+        the star schema and return quaterly sales of employees
+        per location of clients.
+        """
+        country = self.ui.country_cb.currentData()
+        _country = country[0]
+
+        city = self.ui.city_cb.currentData()
+        _city = city[0]
+
+        sql_1 = ( """
+            SELECT cu.country, cu.city, sa.firstName, sa.lastName, sa.managerName, ca.month, ca.year, sum(sh.quantityOrdered*sh.priceEach)
+            FROM shippedorders sh
+            JOIN salesrepemployee sa ON sa.employeeNumber = sh.salesRepEmployeeNumber
+            JOIN calendar ca ON ca.calendar_key = sh.calendar_key
+            JOIN customers cu on cu.customerNumber = sh.customerNumber
+            WHERE cu.country = '""" + _country + """'
+            AND cu.city = '""" + _city + """'
+            GROUP BY sa.firstName, sa.lastName, sa.managerName, ca.month, ca.year
+            ORDER BY sa.firstName, sa.lastName, ca.year, ca.month
+            """ 
+              )
+        
+        sql_2 = ( """
+            SELECT cu.country, cu.city, sa.firstName, sa.lastName, sa.managerName, ca.qtr, ca.year, sum(sh.quantityOrdered*sh.priceEach)
+            FROM shippedorders sh
+            JOIN salesrepemployee sa ON sa.employeeNumber = sh.salesRepEmployeeNumber
+            JOIN calendar ca ON ca.calendar_key = sh.calendar_key
+            JOIN customers cu on cu.customerNumber = sh.customerNumber
+            WHERE cu.country = '""" + _country + """'
+            AND cu.city = '""" + _city + """'
+            GROUP BY sa.firstName, sa.lastName, sa.managerName, ca.qtr, ca.year
+            ORDER BY sa.firstName, sa.lastName, ca.year, ca.qtr
+            """ 
+              )
+
+        if self.ui.monthly_radio_location.isChecked():
+            sql = sql_1
+        elif self.ui.quarterly_radio_location.isChecked():
+            sql = sql_2
+        
+        rows, count = do_query(sql)
+        
+        # Set the sales data into the table cells.
+        row_index = 0
+        for row in rows:
+            # print(row)
+            column_index = 0
+            i = 0
+            for data in row:
+                string_item = str(data)
+                if i == 7:
+                    string_item = "${:,.2f}".format(data)
+                item = QTableWidgetItem(string_item)
+                self.ui.sales_table_location.setItem(row_index, column_index, item)
+                column_index += 1
+                i += 1
+
+            row_index += 1
+                
+        self._adjust_column_widths_location()
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
